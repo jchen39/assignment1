576b1bb4707082b8460008a5b287ed0ec69bc846
Jason <43418785+jchen39@users.noreply.github.com>
2021-10-25 23:45:53 -0400

    added revisions

--- main.rb
+++ main.rb
@@ -1,13 +1,23 @@
+# Jason Chen
+# CSC415 Assignment 1
+
+# The following program takes input files relating to FYS courses and student selections
+# and reads in the files. An scheduling algorithm runs and then then schedules students
+# courses based on their selections. The output files display information relating to
+# each course and the students that have been enrolled in FYS courses
+
 require 'csv'
 
 CourseInfo = []
 StudentInfo = []
+SID = []
+Selection = []
 
 class Create
 
     CID = []
     CNUM = []
-    Title = 
+    Title = []
 
     # Populates our input arrays with the data from the csv file with course id, section, and names
     # Then each course is instantiated with their corresponding values
@@ -16,13 +26,13 @@ class Create
             puts "Enter the name of the course list file(include .csv):"
             listFile = gets.chomp
 
-            CSV.foreach(("FYS-course-list.csv"), headers: true, col_sep: ",") do |col|
+            CSV.foreach((listFile), headers: true, col_sep: ",") do |col|
                 CID << col[0]
                 CNUM << col[1]
                 Title << col[2]
             end
-        rescue
-            puts "File cannot be found, try again:"
+        rescue Errno::ENOENT
+            puts "File cannot be found, try again"
             create_courses
         end
 
@@ -30,10 +40,8 @@ class Create
         for i in 0..CID.length - 1
             CourseInfo[i] = Course.new(CID[i], CNUM[i], Title[i], 0, 18)
         end
+		
     end
-
-    SID = []
-    Selection = []
 
     # Populates our input arrays with the data from the csv file with student id and selections
     # Then each student is instantiated with their ids and course choices
@@ -42,12 +50,12 @@ class Create
             puts "Enter the name of the students & selections file(include .csv):"
             selectFile = gets.chomp
         
-            CSV.foreach(("FYS-Selections.csv"), headers: true, col_sep: ",") do |col|
+            CSV.foreach((selectFile), headers: true, col_sep: ",") do |col|
                 SID << col[0]
                 Selection << col[1]
             end
-        rescue
-            puts "File cannot be found, try again:"
+        rescue Errno::ENOENT
+            puts "File cannot be found, try again"
             create_students
         end
 
@@ -107,13 +115,13 @@ class Student
         if @course
             return true
         end
-        return false
     end
 
 end
 
 # Checks the list of courses available
 # If the course is open, assign the student to that course
+# This is the "any" option for a student
 def assign_to_open(course, student)
     for i in 0..course.length - 1
         if course[i].at_capacity
@@ -153,11 +161,26 @@ end
 
 def get_input
     
-    puts "How many FYS are offered?"
-    numCourses = gets.chomp
+    begin
+        puts "How many FYS are offered?"
+        numCourses = gets.chomp.to_i
 
-    puts "How many students are in the incoming class?"
-    numStudents = gets.chomp
+        # Checks if input is valid(at least one course and a integer value)
+        if(numCourses <= 0 || !numCourses.integer?)
+            raise StandardError
+        end
+
+        puts "How many students are in the incoming class?"
+        numStudents = gets.chomp.to_i
+
+        # Checks if input is valid(at least one student and a integer value)
+        if(numStudents <= 0 || !numStudents.integer?)
+            raise StandardError
+        end
+    rescue StandardError
+        puts "Input not accepted, try again"
+        get_input
+    end
 
 end
 
@@ -175,21 +198,67 @@ def get_output
     
     CSV.open(out1, 'wb') do |csv|
         csv << ["Course ID", "Student ID"]
+		CourseInfo.each do |course|
+			StudentInfo.each do |student|
+				if student.choices == course.cID
+					 csv << [course.cID,student.sID]
+				end
+			end
+		end
     end
 
+	count=0
+	courseCount =0
+	lessCourseCount =0
     CSV.open(out2, 'wb') do |csv|
         csv << ["Course ID", "Course Number", "Course Title", "Student ID", "Under 10?"]
+		CourseInfo.each do |course|
+			StudentInfo.each do |st|
+				if st.choices == course.cID
+					count=count+1
+				end
+			end
+			StudentInfo.each do |student|
+				if student.choices == course.cID
+					if count>=10
+						csv << [course.cID,course.cNum,course.cName,student.sID,"NO"]
+					else
+						csv << [course.cID,course.cNum,course.cName,student.sID,"YES"]
+					end
+				end
+			end
+			if(count>=10)
+				courseCount=courseCount+1;
+			else
+				lessCourseCount=lessCourseCount+1;
+			end
+			count=0
+		end
     end
 
+	notEnrolled =0
+	cntStd = 0
+	StudentInfo.each do |student|
+		StudentInfo.each do |std|
+			if std.sID==student.sID
+				cntStd=cntStd+1;
+			end
+		end
+		if(cntStd<6)
+			notEnrolled=notEnrolled+1;
+		end
+	end
     output = File.new(out3, "w")
-    output.puts("The number of students enrolled in a course:")
-    output.puts("The number of students not enrolled in a course:")
-    output.puts("The number of courses that can run:")
-    output.puts("The number of courses with fewer than 10 students:")
+	uni = SID.uniq
+    output.puts("The number of students enrolled in a course:#{uni.length()}")
+    output.puts("The number of students not enrolled in a course:#{notEnrolled}")
+    output.puts("The number of courses that can run:#{courseCount}")
+    output.puts("The number of courses with fewer than 10 students:#{lessCourseCount}")
 
     puts "All files have been created. Please check your current directory"
 
 end
+
 
 get_input
 c= Create.new
